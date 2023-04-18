15849dd2b98701441c86ab04561ee4cbe0a432ad
JorgeHabib <jorgehabib.khouri@gmail.com>
2023-04-17 22:02:26 -0300

    commit

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import mqtt from 'mqtt'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+// Broker MQTT V3.1.1
+// IP: 143.107.102.8
+// Port: 1883
+// Username: automacao-2023
+// Password: auto835192
+// Topic: garsoft/dev/je05/dados
+// TLS: none
+
+// Connect to MQTT broker with credentials above
+const host = '143.107.102.8'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+const topic = 'garsoft/dev/je05/dados'
+const connectUrl = `mqtt://${host}:${port}`
+
+const data: any = []
+
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'automacao-2023',
+  password: 'auto835192',
+  reconnectPeriod: 1000,
+})
+
+client.on('connect', () => {
+  console.log('[MQTT] Client connected to HOST')
+  client.subscribe([topic], () => {
+    console.log(`[MQTT] Subscribe to topic '${topic}'`)
+  })
+
+  client.on('message', (topic, payload) => {
+    console.log('Received Message:', topic, JSON.stringify(JSON.parse(payload.toString()), null, 2))
+    data.push({
+      ...JSON.parse(payload.toString()).DATA,
+      created_at: new Date()
+    })
+  })
+})
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  return res.json({
+    data
+  })
+})
+
+app.listen(8088, () => {
+  console.log('app running on port 8088')
+})
